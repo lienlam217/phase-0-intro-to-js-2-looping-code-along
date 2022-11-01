1650690fc25845542a8f818ec671b4724d7f550c
Lien Nguyen <lienlam217@gmail.com>
2022-11-01 12:02:14 -0500

    commit tree f828f76626cafc1ec72853627f5f8b115a32a349

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+// code your solutions in this file
+console.log(writeCards(["Sassy", "Amy", "Vinny", "John", "Lien"], "birthday")) 
+
+function writeCards(name, event) {
+    const outPut = []
+    for (let i = 0; i < name.length; i++) {
+       
+       outPut.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return outPut;
+
+}
+
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+    console.log(countDown--);
+}
+
+}
+
+
